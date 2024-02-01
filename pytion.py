4ea7be4fc6a2bf1120fb6e57cd9a234faf026bfa
khalilovsherkhan <xalilovsherxon878@gmail.com>
2024-02-01 21:57:05 +0500

    add

---
+++ pytion.py
@@ -0,0 +1,59 @@
+# name = "❤️"
+
+# print(len(name))
+# print(name.find("o"))
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("o"))
+# print(name.replace("o", "a"))
+# print(name*25000000)
+#  yangi kod
+# x = 1
+# y = 2.0
+# z = "3"
+
+
+# print(x)
+# print(y)
+# print(z*3)
+
+
+# 2-yang kod
+# name = input("ismingiz?: ")
+# age = int(input("yoshingiz nechida?: "))
+# heigth = float(input("boyigiz:?"))
+# meva = input("Yoqtirgan mevagiz:?")
+
+# print("salom , " +name)
+# print("Meni yoshim , " +str(age)+  "  daaa")
+# print("Meni boyim , " +str(heigth)+  "  santimetr")
+# print("Menga , " +str(meva)+  " judda yoqadi")
+
+# 3-kod
+# import math
+# pi = 3.15
+
+
+# print(round(pi))
+# print(math.ciel(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,2))
+# print(math.sqrt(420))
+
+
+
+
+
+# 4-
+
+name = "Broo code"
+
+first_name = name[:2]
+last_name = name[0:2]
+
+print(first_name)
+
+
